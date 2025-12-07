840355093160b10641a831953f18c0b14c7bf9de
ThaoN <591658@student.saxion.nl>
2025-12-07 17:23:13 +0100

    Revert "Test mute mode in GameOverScreen."
    This reverts commit b2d412530007af25e5c6152ae447aeedd80ac6fc.

---
+++ src/main/java/nl/saxion/game/sugarshower/LevelManager.java
@@ -0,0 +1,14 @@
+package nl.saxion.game.sugarshower;
+
+import java.util.ArrayList;
+
+public class LevelManager {
+    int currentLevel = 0;
+    ArrayList<Recipe> allRecipes = new ArrayList<>();
+
+
+}
+
+
+
+
